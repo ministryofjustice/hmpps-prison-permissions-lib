38c598fa991e00d7673ac5b4448df017d8cbc46e
Jon Brighton <brightonsbox@hotmail.com>
2025-05-09 16:32:48 +0100

    CDPS-1345: Updating the case notes check (#25)
    * CDPS-1345: Removing requirement to retrieve keyworker data
    
    * CDPS-1345: Updating the case notes check
    
    So that Global Search users cannot access case notes for someone
    transferring.

--- src/services/permissions/checks/domains/person/caseNotes/CaseNotesReadAndEditCheck.ts
+++ src/services/permissions/checks/domains/person/caseNotes/CaseNotesReadAndEditCheck.ts
@@ -1,30 +1,50 @@
 import { CaseNotesPermission } from '../../../../../../types/permissions/domains/person/caseNotes/CaseNotesPermissions'
 import { PermissionCheckStatus } from '../../../../../../types/permissions/PermissionCheckStatus'
 import PermissionsCheckRequest from '../../../PermissionsCheckRequest'
-import { isInUsersCaseLoad, logDeniedPermissionCheck, userHasAllRoles } from '../../../../utils/PermissionUtils'
+import {
+  isInUsersCaseLoad,
+  logDeniedPermissionCheck,
+  userHasAllRoles,
+  userHasRole,
+} from '../../../../utils/PermissionUtils'
 import restrictedPatientStatus from '../../../baseCheck/status/RestrictedPatientStatus'
 import releasedPrisonerStatus from '../../../baseCheck/status/ReleasedPrisonerStatus'
-import transferringPrisonerStatus from '../../../baseCheck/status/TransferringPrisonerStatus'
 import { Role } from '../../../../../../types/user/Role'
 
 export default function caseNotesReadAndEditCheck(permission: CaseNotesPermission, request: PermissionsCheckRequest) {
   const baseCheckPassed = request.baseCheckStatus === PermissionCheckStatus.OK
 
-  const check = baseCheckPassed && accessOutsideOfCaseLoadDueToGlobalSearchAlsoRequiresPomRole(request)
+  const caseNotesCheckStatus = checkCaseNotesAccess(request)
+  const caseNotesCheckPassed = caseNotesCheckStatus === PermissionCheckStatus.OK
 
-  if (!check) logDeniedPermissionCheck(permission, request, PermissionCheckStatus.ROLE_NOT_PRESENT)
+  const check = baseCheckPassed && caseNotesCheckPassed
+
+  if (!check) logDeniedPermissionCheck(permission, request, caseNotesCheckStatus)
 
   return check
 }
 
-function accessOutsideOfCaseLoadDueToGlobalSearchAlsoRequiresPomRole(request: PermissionsCheckRequest): boolean {
+function checkCaseNotesAccess(request: PermissionsCheckRequest): PermissionCheckStatus {
   const { user, prisoner } = request
-  const inUsersCaseLoad = isInUsersCaseLoad(prisoner.prisonId, user)
 
-  if (prisoner.restrictedPatient) return restrictedPatientStatus(user, prisoner) === PermissionCheckStatus.OK
-  if (prisoner.prisonId === 'OUT') return releasedPrisonerStatus(user) === PermissionCheckStatus.OK
-  if (prisoner.prisonId === 'TRN') return transferringPrisonerStatus(user) === PermissionCheckStatus.OK
-  if (inUsersCaseLoad) return true
+  // Restricted patients follows the base check rules:
+  if (prisoner.restrictedPatient) return restrictedPatientStatus(user, prisoner)
 
+  // Released prisoners follows the base check rules:
+  if (prisoner.prisonId === 'OUT') return releasedPrisonerStatus(user)
+
+  // Case notes are only accessible for transferring prisoners if the user has the Inactive Bookings role:
+  if (prisoner.prisonId === 'TRN') {
+    return userHasRole(Role.InactiveBookings, user.userRoles)
+      ? PermissionCheckStatus.OK
+      : PermissionCheckStatus.PRISONER_IS_TRANSFERRING
+  }
+
+  // Case notes are accessible if the prisoner's prison is in the user's caseload:
+  if (isInUsersCaseLoad(prisoner.prisonId, user)) return PermissionCheckStatus.OK
+
+  // Case notes for prisoners outside the user's caseload are only accessible with both Global Search and POM roles:
   return userHasAllRoles([Role.GlobalSearch, Role.PomUser], user.userRoles)
+    ? PermissionCheckStatus.OK
+    : PermissionCheckStatus.ROLE_NOT_PRESENT
 }
--- src/services/permissions/checks/domains/person/caseNotes/CaseNotesReadAndEditScenarios.ts
+++ src/services/permissions/checks/domains/person/caseNotes/CaseNotesReadAndEditScenarios.ts
@@ -1,26 +1,37 @@
-import { TestScenarios } from '../../../../../../testUtils/TestScenario'
+import { TestScenarios, userWithActiveCaseLoad } from '../../../../../../testUtils/TestScenario'
 import {
   deniedBaseCheckScenarios,
   grantedCaseLoadCheckScenarios,
   grantedGlobalSearchCheckScenarios,
   grantedReleasedPrisonerCheckScenarios,
   grantedRestrictedPatientCheckScenarios,
-  grantedTransferringPrisonerCheckScenarios,
 } from '../../../baseCheck/BaseCheckTestScenarios'
 import { Role } from '../../../../../../types/user/Role'
 import { PermissionCheckStatus } from '../../../../../../types/permissions/PermissionCheckStatus'
 
-const deniedScenarios: TestScenarios = deniedBaseCheckScenarios.and(
-  grantedGlobalSearchCheckScenarios
-    .withoutUserRoles([Role.PomUser])
-    .withExpectedStatus(PermissionCheckStatus.ROLE_NOT_PRESENT),
-)
+const deniedScenarios: TestScenarios = deniedBaseCheckScenarios
+  .and(
+    grantedGlobalSearchCheckScenarios
+      .withoutUserRoles([Role.PomUser])
+      .withExpectedStatus(PermissionCheckStatus.ROLE_NOT_PRESENT),
+  )
+  .andScenarioWhere(
+    userWithActiveCaseLoad('MDI')
+      .withRoles([Role.GlobalSearch])
+      .accessingTransferringPrisoner()
+      .expectsStatus(PermissionCheckStatus.PRISONER_IS_TRANSFERRING),
+  )
 
 const grantedScenarios = grantedRestrictedPatientCheckScenarios
   .and(grantedReleasedPrisonerCheckScenarios)
-  .and(grantedTransferringPrisonerCheckScenarios)
   .and(grantedCaseLoadCheckScenarios)
   .and(grantedGlobalSearchCheckScenarios.withUserRoles([Role.PomUser]))
+  .andScenarioWhere(
+    userWithActiveCaseLoad('MDI')
+      .withRoles([Role.InactiveBookings])
+      .accessingTransferringPrisoner()
+      .expectsStatus(PermissionCheckStatus.OK),
+  )
 
 // eslint-disable-next-line import/prefer-default-export
 export const caseNotesReadAndEditScenarios = grantedScenarios.and(deniedScenarios)
