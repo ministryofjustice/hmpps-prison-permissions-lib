f973325c866564aba1deb037fa580c5c76735d90
Jon Brighton <brightonsbox@hotmail.com>
2025-07-08 10:42:05 +0100

    CDPS-1460: Correcting diet edit permission

--- src/contractTests/prisonerProfile/scenarios/domains/person/personHealthAndMedication/dietEdit/DietEditScenarios.ts
+++ src/contractTests/prisonerProfile/scenarios/domains/person/personHealthAndMedication/dietEdit/DietEditScenarios.ts
@@ -1,5 +1,56 @@
 import { Role } from '../../../../../../../types/internal/user/Role'
-import baseCheckAndUserHasRoleScenarios from '../../../../shared/BaseCheckAndUserHasRoleScenarios'
+import { TestScenarios, userWithActiveCaseLoad } from '../../../../../../../testUtils/TestScenario'
+import {
+  deniedBaseCheckScenarios,
+  grantedGlobalSearchCheckScenarios,
+  grantedReleasedPrisonerCheckScenarios,
+  grantedTransferringPrisonerCheckScenarios,
+} from '../../../../../../../services/permissions/checks/baseCheck/BaseCheckScenarios'
+import { PermissionCheckStatus } from '../../../../../../../types/internal/permissions/PermissionCheckStatus'
+
+const deniedScenarios: TestScenarios = deniedBaseCheckScenarios
+  // These granted base check scenarios should be denied because the user must have the prisoner in the active case load:
+  .and(
+    grantedReleasedPrisonerCheckScenarios
+      .withUserRole(Role.DietAndAllergiesEdit)
+      .withExpectedStatus(PermissionCheckStatus.NOT_ACTIVE_CASELOAD),
+  )
+  .and(
+    grantedTransferringPrisonerCheckScenarios
+      .withUserRole(Role.DietAndAllergiesEdit)
+      .withExpectedStatus(PermissionCheckStatus.NOT_ACTIVE_CASELOAD),
+  )
+  .and(
+    grantedReleasedPrisonerCheckScenarios
+      .withUserRole(Role.DietAndAllergiesEdit)
+      .withExpectedStatus(PermissionCheckStatus.NOT_ACTIVE_CASELOAD),
+  )
+  .and(
+    grantedGlobalSearchCheckScenarios
+      .withUserRole(Role.DietAndAllergiesEdit)
+      .withExpectedStatus(PermissionCheckStatus.NOT_ACTIVE_CASELOAD),
+  )
+  .andScenarioWhere(
+    userWithActiveCaseLoad('MDI')
+      .withAdditionalCaseLoads(['LEI'])
+      .withRoles([Role.DietAndAllergiesEdit])
+      .accessingPrisonerAt('LEI')
+      .expectsStatus(PermissionCheckStatus.NOT_ACTIVE_CASELOAD),
+  )
+  // This granted base check scenario should be denied because the user doesn't have the required role:
+  .andScenarioWhere(
+    userWithActiveCaseLoad('MDI')
+      .withRoles([])
+      .accessingPrisonerAt('MDI')
+      .expectsStatus(PermissionCheckStatus.ROLE_NOT_PRESENT),
+  )
+
+const grantedScenarios = new TestScenarios([
+  userWithActiveCaseLoad('MDI')
+    .withRoles([Role.DietAndAllergiesEdit])
+    .accessingPrisonerAt('MDI')
+    .expectsStatus(PermissionCheckStatus.OK),
+])
 
 // eslint-disable-next-line import/prefer-default-export
-export const dietEditScenarios = baseCheckAndUserHasRoleScenarios(Role.DietAndAllergiesEdit)
+export const dietEditScenarios = grantedScenarios.and(deniedScenarios)
--- src/services/permissions/checks/domains/person/personHealthAndMedication/dietEdit/DietEditCheck.ts
+++ src/services/permissions/checks/domains/person/personHealthAndMedication/dietEdit/DietEditCheck.ts
@@ -1,10 +1,26 @@
 import PermissionsCheckRequest from '../../../../PermissionsCheckRequest'
-import baseCheckAndUserHasRole from '../../../../sharedChecks/baseCheckAndUserHasRole/BaseCheckAndUserHasRole'
 import { PersonHealthAndMedicationPermission } from '../../../../../../../types/public/permissions/domains/person/personHealthAndMedication/PersonHealthAndMedicationPermissions'
 import { Role } from '../../../../../../../types/internal/user/Role'
+import { PermissionCheckStatus } from '../../../../../../../types/internal/permissions/PermissionCheckStatus'
+import { isActiveCaseLoad, logDeniedPermissionCheck, userHasRole } from '../../../../../utils/PermissionUtils'
 
 const permission = PersonHealthAndMedicationPermission.edit_diet
 
 export default function dietEditCheck(request: PermissionsCheckRequest) {
-  return baseCheckAndUserHasRole(Role.DietAndAllergiesEdit, permission, request)
+  const { user, prisoner, baseCheckStatus } = request
+
+  const baseCheckPassed = baseCheckStatus === PermissionCheckStatus.OK
+  const inActiveCaseLoad = isActiveCaseLoad(prisoner.prisonId, user)
+  const hasRole = userHasRole(Role.DietAndAllergiesEdit, user)
+
+  const check = baseCheckPassed && inActiveCaseLoad && hasRole
+
+  if (!check)
+    logDeniedPermissionCheck(
+      permission,
+      request,
+      inActiveCaseLoad ? PermissionCheckStatus.ROLE_NOT_PRESENT : PermissionCheckStatus.NOT_ACTIVE_CASELOAD,
+    )
+
+  return check
 }
