7654b0d39c67d58e27663e1fdc1cfeba5e8540d4
Michael Clancy <michael.clancy@digital.justice.gov.uk>
2025-10-06 11:25:07 +0100

    CDPS-1572: Updated permissions check for sensitive case notes.

--- src/services/permissions/checks/domains/person/caseNotes/sensitiveCaseNotesDelete/SensitiveCaseNotesDeleteCheck.ts
+++ src/services/permissions/checks/domains/person/caseNotes/sensitiveCaseNotesDelete/SensitiveCaseNotesDeleteCheck.ts
@@ -1,10 +1,10 @@
 import PermissionsCheckRequest from '../../../../PermissionsCheckRequest'
 import { CaseNotesPermission } from '../../../../../../../types/public/permissions/domains/person/caseNotes/CaseNotesPermissions'
 import { Role } from '../../../../../../../types/internal/user/Role'
-import baseCheckAndUserHasRole from '../../../../sharedChecks/baseCheckAndUserHasRole/BaseCheckAndUserHasRole'
+import caseNotesReadAndEditCheckAndUserHasRolesFrom from '../CaseNotesReadAndEditCheckAndUserHasSomeRolesFrom'
 
 const permission = CaseNotesPermission.delete_sensitive
 
 export default function sensitiveCaseNotesDeleteCheck(request: PermissionsCheckRequest) {
-  return baseCheckAndUserHasRole(Role.DeleteSensitiveCaseNotes, permission, request)
+  return caseNotesReadAndEditCheckAndUserHasRolesFrom([Role.DeleteSensitiveCaseNotes], permission, request)
 }
