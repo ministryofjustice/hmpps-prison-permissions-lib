ba25fa9c2c76b4d66a0663602885fb2f56d35ebc
Ewan-Donovan <ewan.donovan@digital.justice.gov.uk>
2026-02-04 14:11:39 +0000

    Logic tweak

--- src/services/permissions/checks/sharedChecks/contactsReadCheck/contactsReadCheck.ts
+++ src/services/permissions/checks/sharedChecks/contactsReadCheck/contactsReadCheck.ts
@@ -1,15 +1,21 @@
 import { PermissionCheckStatus } from '../../../../../types/internal/permissions/PermissionCheckStatus'
 import { Role } from '../../../../../types/internal/user/Role'
 import { matchBaseCheckAnd } from '../../../utils/PermissionCheckUtils'
-import { isInUsersCaseLoad, isReleased, userHasSomeRolesFrom, wasReleasedWithinThreeYears } from '../../../utils/PermissionUtils'
+import {
+  isInUsersCaseLoad,
+  isReleased,
+  userHasSomeRolesFrom,
+  wasReleasedWithinThreeYears,
+} from '../../../utils/PermissionUtils'
 
 const contactsReadCheck = matchBaseCheckAnd({
   overridingCondition: (user, prisoner) => {
     if (isReleased(prisoner)) {
-        if (!userHasSomeRolesFrom([Role.ContactsAdministrator, Role.ContactsAuthoriser], user)) return PermissionCheckStatus.ROLE_NOT_PRESENT
-        if (!isInUsersCaseLoad(prisoner.previousPrisonId, user)) return PermissionCheckStatus.NOT_IN_CASELOAD
-        if (!wasReleasedWithinThreeYears(prisoner)) return PermissionCheckStatus.NOT_PERMITTED
-        return PermissionCheckStatus.OK
+      if (!userHasSomeRolesFrom([Role.ContactsAdministrator, Role.ContactsAuthoriser], user))
+        return PermissionCheckStatus.ROLE_NOT_PRESENT
+      if (!isInUsersCaseLoad(prisoner.previousPrisonId, user)) return PermissionCheckStatus.NOT_IN_CASELOAD
+      if (!wasReleasedWithinThreeYears(prisoner)) return PermissionCheckStatus.NOT_PERMITTED
+      return PermissionCheckStatus.OK
     }
 
     return isInUsersCaseLoad(prisoner.prisonId, user) ? PermissionCheckStatus.OK : PermissionCheckStatus.NOT_IN_CASELOAD
