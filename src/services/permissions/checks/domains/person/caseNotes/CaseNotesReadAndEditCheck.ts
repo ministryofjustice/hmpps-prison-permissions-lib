0f761e4091f0d6cc0f1bcd0ba1dfab47f0d9852a
Igor <igor.ushkarev1@justice.gov.uk>
2026-09-20 15:19:51 +0100

    Extract time-based post prisoner transfer check

--- src/services/permissions/checks/domains/person/caseNotes/CaseNotesReadAndEditCheck.ts
+++ src/services/permissions/checks/domains/person/caseNotes/CaseNotesReadAndEditCheck.ts
@@ -1,9 +1,7 @@
 import { PermissionCheckStatus } from '../../../../../../types/internal/permissions/PermissionCheckStatus'
-import { isInUsersCaseLoad, userHasAllRoles, userHasRole } from '../../../../utils/PermissionUtils'
+import { checkTimeBasedAccessPostTransfer, userHasAllRoles, userHasRole } from '../../../../utils/PermissionUtils'
 import { Role } from '../../../../../../types/internal/user/Role'
-import { HmppsUser } from '../../../../../../types/internal/user/HmppsUser'
-import Prisoner from '../../../../../../data/hmppsPrisonerSearch/interfaces/Prisoner'
-import { daysToMilliseconds, isDateWithinBounds } from '../../../../utils/DateUtils'
+import { daysToMilliseconds } from '../../../../utils/DateUtils'
 import { matchBaseCheckAnd } from '../../../../utils/PermissionCheckUtils'
 import { PrisonerPermissionConditions } from '../../../../PrisonerPermissionConditions'
 
@@ -20,8 +18,8 @@ export const caseNotesReadAndEditConditions: Partial<PrisonerPermissionCondition
     if (!userHasAllRoles([Role.GlobalSearch, Role.PomUser], user)) return PermissionCheckStatus.ROLE_NOT_PRESENT
 
     // Case notes for prisoners outside the user's caseload are only accessible with both Global Search and POM roles
-    // if the prisoner was previously in one of the users caseloads in the 30 days:
-    return checkTimeBasedAccessToCaseNotesPostTransfer(user, prisoner, caseNotesAccessPeriodPostTransferInMs)
+    // if the prisoner was previously in one of the users caseloads in the last 90 days:
+    return checkTimeBasedAccessPostTransfer(user, prisoner, caseNotesAccessPeriodPostTransferInMs)
       ? PermissionCheckStatus.OK
       : PermissionCheckStatus.NOT_PERMITTED
   },
@@ -30,16 +28,3 @@ export const caseNotesReadAndEditConditions: Partial<PrisonerPermissionCondition
 const caseNotesReadAndEditCheck = matchBaseCheckAnd(caseNotesReadAndEditConditions)
 
 export default caseNotesReadAndEditCheck
-
-function checkTimeBasedAccessToCaseNotesPostTransfer(
-  user: HmppsUser,
-  prisoner: Prisoner,
-  timePeriodForAccessPostTransferInMilliseconds: number,
-): boolean {
-  if (!isInUsersCaseLoad(prisoner.previousPrisonId, user) || !prisoner.previousPrisonLeavingDate) return false
-
-  const previousPrisonLeavingDate = Date.parse(prisoner.previousPrisonLeavingDate)
-  const today = Date.now()
-
-  return isDateWithinBounds(previousPrisonLeavingDate, today, today - timePeriodForAccessPostTransferInMilliseconds)
-}
