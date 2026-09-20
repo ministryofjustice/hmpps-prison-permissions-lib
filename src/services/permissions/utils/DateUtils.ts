3f25f13935402bf351b526e7bf67cedec0f7afb2
Igor <igor.ushkarev1@justice.gov.uk>
2026-09-20 15:18:31 +0100

    Fix getCurrentDateMinusDaysAsString to handle daylight savings switches better

--- src/services/permissions/utils/DateUtils.ts
+++ src/services/permissions/utils/DateUtils.ts
@@ -1,5 +1,16 @@
+const shortDateFormatter = new Intl.DateTimeFormat('en-GB', {
+  // NB: 'numeric' for day or month always produces leading zeroes so might as well make it explicit
+  day: '2-digit',
+  month: '2-digit',
+  year: 'numeric',
+  timeZone: 'Europe/London',
+})
+
 export function getCurrentDateMinusDaysAsString(currentDateInMs: number, daysToSubtract: number): string {
-  return new Date(currentDateInMs - daysToMilliseconds(daysToSubtract)).toISOString().split('T')[0]
+  const date = new Date(currentDateInMs - daysToMilliseconds(daysToSubtract))
+  // NB: cannot simply `date.toISOString().split('T')[0]` because that returns the UTC date
+  const parts = Object.fromEntries(shortDateFormatter.formatToParts(date).map(part => [part.type, part.value]))
+  return `${parts.year}-${parts.month}-${parts.day}`
 }
 
 export function daysToMilliseconds(days: number): number {
